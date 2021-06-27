5c63906c3d0ff3e04e334cb73a994fbd2d02913c
Iman <imanghafoori1@gmail.com>
2021-06-28 01:37:17 +0430

    Update ExternalIframeController.php

--- src/Http/Controllers/ExternalIframeController.php
+++ src/Http/Controllers/ExternalIframeController.php
@@ -8,8 +8,10 @@ class ExternalIframeController
 {
     public function __invoke(Request $request)
     {
-        if ($request->has(str_replace(['?', '='], '', config('iframes.external_link_key')))) {
-            $iframeSource = $request->{str_replace(['?', '='], '', config('iframes.external_link_key'))};
+        $key = str_replace(['?', '='], '', config('iframes.external_link_key'));
+
+        if ($request->has($key)) {
+            $iframeSource = $request->$key;
 
             return view('iframes::'.config('iframes.theme').'.external-iframe', ['iframeSource' => $iframeSource]);
         }
