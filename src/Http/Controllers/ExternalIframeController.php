047306e7b8601ecb2e14650821aca3a40c85ee3d
inmanturbo <inman.turbo@gmail.com>
2021-06-27 01:38:20 -0400

    fix errors and typos

--- src/Http/Controllers/ExternalIframeController.php
+++ src/Http/Controllers/ExternalIframeController.php
@@ -8,8 +8,8 @@ class ExternalIframeController
 {
     public function __invoke(Request $request)
     {
-        if ($request->has(str_replace(['?', '='], '', config('iframes.external_link_query')))) {
-            $iframeSource = $request->{str_replace(['?', '='], '', config('iframes.external_link_query'))};
+        if ($request->has(str_replace(['?', '='], '', config('iframes.external_link_key')))) {
+            $iframeSource = $request->{str_replace(['?', '='], '', config('iframes.external_link_key'))};
 
             return view('laravel-iframes::'.config('iframes.theme').'.external-iframe', ['iframeSource' => $iframeSource]);
         }
