b2b445f715e3f5267b874dbfbf96a01bf2145efd
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2021-07-14 12:55:06 -0400

    Update iframes.php

--- routes/iframes.php
+++ routes/iframes.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::any('/' . config('iframes.internal_iframe_prefix') . '/{path}', InternalIframeController::class)
     ->where('path', '(.*)')
-    ->middleware(config('iframes.middleware'));
+    ->middleware(config('iframes.middleware'))
+    ->name('internal-iframe');
 
 Route::get('/' . config('iframes.external_iframe_prefix'), ExternalIframeController::class)
-    ->middleware(config('iframes.middleware'));
+    ->middleware(config('iframes.middleware'))
+    ->name('external-iframe');
 
